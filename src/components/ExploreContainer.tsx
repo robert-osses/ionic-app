8a4bc1d60a4237ac7515ed81f824d865641d580f
unknown <Profesor@SCAAOB00LC60300.duoc.acad>
2024-08-29 14:40:27 -0400

    Modifica componente tarjeta

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -6,7 +6,7 @@ function ExploreContainer() {
     <IonCard>
       <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
       <IonCardHeader>
-        <IonCardTitle>Card Title</IonCardTitle>
+        <IonCardTitle>Titulo Tarjeta</IonCardTitle>
         <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
       </IonCardHeader>
 
