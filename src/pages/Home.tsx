22cddb19a42210affbeec92f8a5b70b63e74c94e
unknown <Profesor@SCAAOB00LC60300.duoc.acad>
2024-08-29 14:19:57 -0400

    Inicio de los tiempos de ionic

---
+++ src/pages/Home.tsx
@@ -0,0 +1,32 @@
+import { 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar
+} 
+from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Mi primera Aplicación Móvil</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Contenido de la App</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
